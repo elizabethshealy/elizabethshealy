01e5234179897461652af2eaac9a9002230355b4
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2015-07-31 11:49:33 -0400

    adding in bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -58,11 +58,11 @@ gulp.task('watch', function () {
   watch(src.allScss, function () {
     gulp.start('scss');
   });
-  
+
   watch(src.allImg, function () {
     gulp.start('img');
   });
-  
+
   watch(src.allFont, function () {
     gulp.start('font');
   });
@@ -96,7 +96,7 @@ gulp.task('scss', function () {
 // Build our JavaScript files using browserify
 gulp.task('js', function () {
   var stream;
-  
+
   try {
     stream = browserify('./src/js/init.js', { debug: true })
     .transform('bulkify')
@@ -111,7 +111,7 @@ gulp.task('js', function () {
     console.error(ex);
     return;
   }
-  
+
   return stream
      .on('error', swallowError)
     .pipe(source('app.js'))
