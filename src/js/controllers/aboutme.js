afe1830eecdb0498687f4fea5e5bab67643bd759
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2015-08-28 13:00:30 -0400

    lining up divs on home screen

--- src/js/controllers/aboutme.js
+++ src/js/controllers/aboutme.js
@@ -1,13 +1,29 @@
 'use strict';
 
+var router = require('../router');
 var $ = require('jquery');
 var _ = require('underscore');
 var views = require('views');
-var router = require('../router');
+var pdf = require('../utilities/pdfobject.js');
 
 router.route('aboutme', function () {
-  var activityHTML = views['aboutme'];
 
+  var activityHTML = views['aboutme'];
   $('.main-content').html(activityHTML);
 
 });
+
+
+
+
+
+
+// $(document).ready(function() {
+//     $(".thumbnail-left").click(function () {
+//         $(this).toggleClass('project-thumbnail-image-left');
+//     });
+//
+//     $(".thumbnail-right").click(function () {
+//         $(this).toggleClass('project-thumbnail-image-right');
+//     });
+// });
